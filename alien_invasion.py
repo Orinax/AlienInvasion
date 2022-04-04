eaaaad2073d760f9e78b12064343a32a4ccb1894
Orinax <chaunceypelton@gmail.com>
2022-04-04 23:49:09 +0700

    Game broken after adding level display

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,7 +33,7 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, stats, play_button, ship,
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship,
                         aliens, bullets)
         gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets,
                          play_button)
--- game_functions.py
+++ game_functions.py
@@ -36,7 +36,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, stats, play_button, ship,
+def check_events(ai_settings, screen, stats, sb, play_button, ship,
                  aliens, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
@@ -50,18 +50,18 @@ def check_events(ai_settings, screen, stats, play_button, ship,
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             check_play_button(ai_settings, screen, stats, play_button,
-                              ship, aliens, bullets, mouse_x, mouse_y)
+                              ship, sb, aliens, bullets, mouse_x, mouse_y)
 
 
-def check_play_button(ai_settings, screen, stats, play_button, ship,
+def check_play_button(ai_settings, screen, stats, play_button, ship, sb,
                       aliens, bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks PLay."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
-        start_game(ai_settings, screen, stats, ship, aliens, bullets)
+        start_game(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
 
-def start_game(ai_settings, screen, stats, ship, aliens, bullets):
+def start_game(ai_settings, screen, stats, sb, ship, aliens, bullets):
     # Reset the game settings.
     ai_settings.initialize_dynamic_settings()
 
@@ -71,6 +71,11 @@ def start_game(ai_settings, screen, stats, ship, aliens, bullets):
     # Reset the game satistics.
     stats.reset_stats()
     stats.game_active = True
+
+    # Reset the scoreboard images.
+    sb.prep_score()
+    sb.prep_high_score()
+    sb.prep_level()
 
     # Empty the list of aliens and bullets.
     aliens.empty()
@@ -132,9 +137,14 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
         check_high_score(stats, sb)
 
     if len(aliens) == 0:
-        # Destroy existing bullets, speed up game, and create a new fleet.
+        # If the entire fleet is destroyed, start a new level.
         bullets.empty()
         ai_settings.increase_speed()
+
+        # Increase level.
+        stats.level += 1
+        sb.prep_level()
+
         create_fleet(ai_settings, screen, ship, aliens)
 
 
